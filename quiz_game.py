09191883ec3dee82f48b7c6b8d4b0935beffae24
viskrishnan <krishnansubramanian007@gmail.com>
2024-09-05 18:36:11 +0530

    First Game project

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,14 @@
 print("Welcome to Quiz Game")
+
+playing = input("Do you want to play a game ? ")
+
+if playing.upper() != "YES":
+    quit()
+
+print("Okay ! Lets play the game !")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct Answer!!")
+else:
+    print("Incorrect Answer!")
