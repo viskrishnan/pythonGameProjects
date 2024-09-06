b4e2d1d376367eb1b7e5f2351829fe291dd63b20
viskrishnan <krishnansubramanian007@gmail.com>
2024-09-06 13:19:36 +0530

    First Game project

--- quiz_game.py
+++ quiz_game.py
@@ -6,9 +6,22 @@ if playing.upper() != "YES":
     quit()
 
 print("Okay ! Lets play the game !")
+score = 0
 
 answer = input("What does CPU stand for? ")
 if answer == "central processing unit":
     print("Correct Answer!!")
+    score += 1
 else:
     print("Incorrect Answer!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphical processing unit":
+    print("Correct Answer!!")
+    score += 1
+else:
+    print("Incorrect Answer!")
+
+print(f"You scored {score} points !!")
+
+
---
+++ rock_paper_scissors.py
@@ -0,0 +1,37 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+options = ["r", "p", "s"]
+
+while True:
+    user_input = input("Type R for Rock/P for Paper/S for scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        print("please enter a valid option for this game")
+        continue
+    random_number = random.randint(0, 2)
+#    rock:0 paper:1 , scissors:2
+    computer_pick = options[random_number]
+    print(f"Computer picked {computer_pick} .")
+
+    if user_input == "r" and computer_pick == "s":
+        print("You won !")
+        user_wins += 1
+
+    elif user_input == "p" and computer_pick == "r":
+        print("You won !")
+        user_wins += 1
+
+    elif user_input == "s" and computer_pick == "r":
+        print("You won !")
+        user_wins += 1
+    else:
+        print("you lost :-(")
+        computer_wins += 1
+
+print(f"You won {user_wins} times")
+print(f"computer wins {computer_wins} times")
+print("See ya !! ")
